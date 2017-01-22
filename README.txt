6b034ffc75f37bf77524bd81faf94dd69c23e3ec
Oskar J <o.jonczyk@gmail.com>
2017-01-23 00:38:45 +0100

    Add project files.

---
+++ README.txt
@@ -0,0 +1,10 @@
+autor: Oskar Jonczyk
+
+Aplikacja "Sklep MTG" pozwala u¿ytkownikowi na obejrzenie zestawu kart orz informacji o nich.
+
+Program napisany za pomoc¹ WPF i C#. Program czerpie dane z pliku zewnêtrznego XML.
+W pliku XML ograniczonym przez DTD mo¿emy dodawaæ karty i obrazy z nimi powi¹zane.
+
+Instrukcja:
+1. wybraæ kartê z menu g³ownego i podwójnie klikn¹æ.
+2. Wyœwietl¹ siê informacjê o karcie w nowym oknie.
---
+++ Sklep MTG/Window1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Sklep_MTG
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1(XmlNode node)
+        {
+            InitializeComponent();
+            for(var i = 0; i < node.ChildNodes.Count - 1; ++i)
+            {
+                label1.Content += node.ChildNodes[i].Name.First().ToString().ToUpper()+node.ChildNodes[i].Name.Substring(1)+": ";
+                if (node.ChildNodes[i].InnerText.Length != 0)
+                {
+                    label1.Content += node.ChildNodes[i].InnerText + "\n\n";
+                } else
+                {
+                    label1.Content += node.ChildNodes[i].Attributes[0].Value+"\n\n";
+                }
+            }
+            image1.Source = new BitmapImage( new Uri(node.ChildNodes[4].InnerText, UriKind.RelativeOrAbsolute));
+        }
+    }
+}
